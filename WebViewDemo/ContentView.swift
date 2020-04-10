da9f8c450a5107d499ce20d780d18acb68beab90
Birkan Ovayolu <ovayolu@gmail.com>
2020-04-10 18:06:48 +0100

    Initial Commit

---
+++ WebViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewDemo
+//
+//  Created by Birkan Ovayolu on 10/04/2020.
+//  Copyright © 2020 Aris Education Technologies. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
